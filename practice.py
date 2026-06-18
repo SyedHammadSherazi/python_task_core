a2d0396a49e83ef31e04e96f9d780f46efca2ae8
SyedHammadSherazi <144503092+SyedHammadSherazi@users.noreply.github.com>
2026-06-18 16:05:34 +0500

    duplicates_name_remove

--- practice.py
+++
@@ -1,7 +0,0 @@
-# fruits=["apple", "orange", "apple", "banana"]
-# print(len(fruits))
-
-
-fruits=["apple", "orange", "banana"]
-fruits[1]= "Mango"
-print(fruits)
---
+++ task_9.py
@@ -0,0 +1,4 @@
+# Remove duplicate values from a list (hint: use a set)
+students=["Ali", "Ahmad", "Jameel", "Ahmad", "Arsalan"]
+rem_duplicate=list(set(students))
+print(rem_duplicate)
