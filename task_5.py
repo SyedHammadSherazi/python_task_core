6f15be3b9564d3e56933ec10e89e98bcd97a5b42
SyedHammadSherazi <144503092+SyedHammadSherazi@users.noreply.github.com>
2026-06-18 09:28:59 +0500

    modify according to user value . first only hard code value used

--- task_5.py
+++ task_5.py
@@ -1,14 +1,6 @@
 # Check whether a given number is even or odd. 
-number=30
-if number % 2 == 0 :
-    print("number is Even")
+number= int(input("Enter the number: "))
+if number % 2 == 0:
+    print("the number is even")
 else:
-    print("number is odd" )
-
-
-    # 
-    number2=23
-    if number % 2 == 0 :
-        print("number is odd")
-    else:
-        print("number is even")
+    print("the number is odd")
