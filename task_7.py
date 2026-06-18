fc2cddee76ed8283fa9cfc5ef4380a10295a899c
SyedHammadSherazi <144503092+SyedHammadSherazi@users.noreply.github.com>
2026-06-18 15:43:33 +0500

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -1,8 +1,26 @@
-tupel1=("orange","mango")
-list_tuple=list(tupel1)
-list_tuple[1]="banana"
-tupel1=tuple(list_tuple)
-print(tupel1)
-thistuple = ("apple", "banana", "cherry")
-for i in range(len(thistuple)):
-  print(thistuple[i])
+# Find the largest and smallest number in a list — first with max()/min(), then without
+# them
+number_largest=[1, 2, 3, 55, 69]
+
+max1 = number_largest[0]
+min1 = number_largest[0]
+for num in number_largest:
+  if num>max1:
+   max1 = num
+print("the maximum value is : ", max1)
+
+for mins in number_largest:
+  if mins<min1:
+   min1 = mins
+print("the minimum value is : ", min1)
+
+  
+
+print("using with min and max function")
+
+number_largest1=[23,44,55]
+nums=max(number_largest1)
+print("The maximum value is :",nums)
+number_largest2=[2,2,1,0,100,-1]
+numbers=min(number_largest2)
+print("minimum value is:", numbers)
