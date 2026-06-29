e492f9d7cdd623755a17ffd39958920aa2aeec7a
SyedHammadSherazi <144503092+SyedHammadSherazi@users.noreply.github.com>
2026-06-29 10:32:38 +0500

    Refactor prime number function and update calls

--- Functions/function_task_1.py
+++ Functions/function_task_1.py
@@ -1,13 +1,13 @@
-# Write a function is_prime(n) that returns whether a number is prime
-def _prime_num(n):
-    if n==1:
+# Write a function is_prime(n) that returns whether a number is prime  
+def is_prime(n):
+    if n <= 1:
         return False
-    elif n==2:
-        return True
-    else:
-     for x in range(2,n):
-        if n % x==0:
+
+    for i in range(2, n):
+        if n % i == 0:
             return False
-        return True
-print(_prime_num(7)) 
-# print(_prime_num(8))     
+
+    return True
+
+print(is_prime(7))   # True
+print(is_prime(8))   # False
